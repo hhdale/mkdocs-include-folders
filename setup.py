14fcef100e45513bb05a91568b5169715dba7362
Håkon Haugholt-Dale <135810534+hhdale@users.noreply.github.com>
2023-06-08 13:43:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(name):
 
 setuptools.setup(
     name='mkdocs-include-folders',
-    version='0.1.2',
+    version='0.1.3',
     packages=['mkdocs_include_folders'],
     url='https://github.com/hhdale/mkdocs-include-folders',
     license='Apache',
